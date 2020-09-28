bfe682a6e22793b35562b26ceb0f9c961b75f390
Hugo Vallada <valladahugo@gmail.com>
2020-09-28 15:31:14 -0300

    rc

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ import { handleSignIn } from './controllers/signInController.js';
 import { handleProfile } from './controllers/profileController.js'
 import {handleImage, handleApiCall} from './controllers/imageController.js';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
     connectString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
